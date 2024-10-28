116f4ca8f7e42982208c0e6c126f269a8c463a0a
Mazunki Hoksaas <rolferen@gmail.com>
2024-10-28 19:36:14 +0100

    support timeout

--- ruter_api/api/departures.py
+++ ruter_api/api/departures.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
+import os
+import json
+import time
 import datetime
-from typing import List, Set
-
-from .routes import Route
-from ..config import API_ENDPOINT, APP_NAME
-
 import requests
+from typing import Set
+from .routes import Route
+from ..config import *
 
 class Departure:
     def __init__(self, arrival_time: datetime.datetime, departure_time: datetime.datetime, destination: str, route: Route):
@@ -73,7 +74,6 @@ class DepartureQuery:
     departures: Set
 
     def __init__(self, station_id: str, number_of_departures: int = 5, time_range: int = 7200):
-
         if not station_id.startswith("NSR:StopPlace"):
             from .stations import GeocodeQuery
             station_id = GeocodeQuery(station_id)[0].station_id
@@ -81,6 +81,7 @@ class DepartureQuery:
         self.station_id = station_id
         self.number_of_departures = number_of_departures
         self.time_range = time_range
+        self.cache_file = os.path.join(CACHE_DIR, f"departures_{station_id}.json")
 
     @property
     def graphql_query(self) -> str:
@@ -109,15 +110,34 @@ class DepartureQuery:
         }}
         """
 
-    def get_data(self):
-        headers = {
-            "Content-Type": "application/json",
-            "ET-Client-Name": APP_NAME
-        }
+    def is_cache_valid(self) -> bool:
+        if not os.path.exists(self.cache_file):
+            return False
+        last_modified = os.path.getmtime(self.cache_file)
+        return (time.time() - last_modified) < CACHE_EXPIRATION
 
-        response = requests.post(API_ENDPOINT, json={"query": self.graphql_query}, headers=headers)
-        response.raise_for_status()
-        self.data = response.json()
+    def load_from_cache(self):
+        with open(self.cache_file, 'r') as f:
+            self.data = json.load(f)
+
+    def save_to_cache(self):
+        if not os.path.exists(CACHE_DIR):
+            os.mkdir(CACHE_DIR)
+        with open(self.cache_file, 'w') as f:
+            json.dump(self.data, f)
+
+    def get_data(self):
+        if self.is_cache_valid():
+            self.load_from_cache()
+        else:
+            headers = {
+                "Content-Type": "application/json",
+                "ET-Client-Name": APP_NAME
+            }
+            response = requests.post(API_ENDPOINT, json={"query": self.graphql_query}, headers=headers)
+            response.raise_for_status()
+            self.data = response.json()
+            self.save_to_cache()
 
     def __iter__(self):
         if not hasattr(self, "data"):
@@ -131,10 +151,4 @@ class DepartureQuery:
 
     def __repr__(self):
         return f"DepartureQuery(station_id={self.station_id}, number_of_departures={self.number_of_departures}, time_range={self.time_range})"
-
-    def __str__(self):
-        if not hasattr(self, "data"):
-            self.get_data()
-
-        return str(list(iter(self)))
 
--- ruter_api/config.py
+++ ruter_api/config.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python
 
-APP_NAME = "mazunki_dev-waybar_widget"
+AUTHOR = "mazunki_dev"
+_APP_NAME = "transport_widget"
+APP_NAME = f"{AUTHOR}-${_APP_NAME}"
 
 API_ENDPOINT = "https://api.entur.io/journey-planner/v3/graphql"
 GEOCODER_ENDPOINT = "https://api.entur.io/geocoder/v1/autocomplete"
+
+def get_cache_dir():
+    import os
+    return os.path.join(
+        os.environ.get("XDG_CACHE_HOME") or
+        os.path.join(os.environ["HOME"], ".cache"),
+        _APP_NAME
+    )
+
 
 HEADERS = {
     "Content-Type": "application/json",
     "ET-Client-Name": "mazunki_ruter_api-waybar",
 }
+
+CACHE_EXPIRATION = 10*60
+CACHE_DIR = get_cache_dir()
+
+
 
